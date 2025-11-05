7961aafaf327826a934d057c65ce33f36782c3df
danuandrisaputra090903-png <danuandrisaputra090903@gmail.com>
2025-11-05 21:12:18 +0800

    Add files via upload

---
+++ tugas1.py
@@ -0,0 +1,27 @@
+# belajar variabel 
+# cara kita menyimpan nilai
+
+# memberikan nilai (assigment)
+x = 10
+y = 6
+lebar = 13
+
+print(x + y + lebar)
+print("nilai dari x =", x)
+print("nilai dari y =, y")
+print("nilai dari lebar =", lebar)
+
+# tings to consider when making a variable
+# hal2 yang harus diperhatikan ketika membuat variabel
+
+bet99 = 9 # tidak boleh diawali dengan angka 
+jumlah_hutang = 1000000 # menggunakan data dua suku kata harus digabungkan dengan_
+jumlah_hutang = 1000000 # menggunakan data dua suku kata harus digabungkan dengan huruf kapital
+
+# panggilan indirect
+z = lebar
+print("nilai dari z =", z)
+
+# tugas 
+# nama saya adalah : (danu saputra)
+# nim saya adalah : (25241099)
